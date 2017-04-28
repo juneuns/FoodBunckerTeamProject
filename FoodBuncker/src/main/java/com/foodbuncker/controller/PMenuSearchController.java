635aa1f1e935369ed280e491908c2fe9795b7950
sss <sss@PC502_16>
2017-04-28 12:02:10 +0900

    pull test

--- FoodBuncker/src/main/java/com/foodbuncker/controller/PMenuSearchController.java
+++ FoodBuncker/src/main/java/com/foodbuncker/controller/PMenuSearchController.java
@@ -1,14 +1,24 @@
 package com.foodbuncker.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.foodbuncker.service.PMenuSearchService;
+import com.foodbuncker.vo.PMenuSearchVO;
+
 @Controller
 public class PMenuSearchController {
+	@Autowired
+	PMenuSearchService service;
 	
 	@RequestMapping("/person/Menu.food")
 	public ModelAndView menuView(ModelAndView mv){
+		ArrayList<PMenuSearchVO> list = service.selectMenu();
+		mv.addObject("LIST",list);
 		mv.setViewName("/person/Menu");
 		return mv;
 	}
